52cbf57aeafc55d816a85347a7b364b7f6a11a3e
nextbox <giridharank790@gmail.com>
2026-03-10 20:37:18 +0530

    Fix syntax errors in discount calculation logic
    Signed-off-by: nextbox <giridharank790@gmail.com>

--- program6.cpp
+++ program6.cpp
@@ -6,11 +6,11 @@ float amt, p;
 clrscr();
 cout<< "\n Enter the Purchase amount...";
 cin>> amt;
-if $(amt>1\&8~amt<5000)\{$
+if $(amt>1\&8~amt<5000)\{
 cout<<"\n No discount for purchase below 5000";
 cout<<"\n Thank you.. Visit again";
-}else if $(amt>=500088~amt<10000)$
-$\{p=amt^{*}5/100;$
+}else if (amt>=500088~amt<10000)
+\{p=amt^{*}5/100;
 cout<<"\n Your purchase has 5% discount";
 cout<<"\n Your discount amount:"<<p;
 cout<<"\n Thank you... Visit again";
@@ -19,7 +19,7 @@ $\{p=amt^{*}10/100;$
 cout<<"\n Your purchase has 10% discount";
 cout<<"\n Your discount amount:"<<p;
 cout<<"\n Thank you... Visit again";
-}else if $(amt>15000)$
+}else if (amt>15000)
 {cout<<"\n Purchase value exceeds";
 }else{
 cout<<"\n Invalid Purchase";}
