9fb930e803374f5849526eda6fced0d50c0b222b
nextbox <giridharank790@gmail.com>
2026-03-10 12:05:40 +0530

    Create program7-2.php

---
+++ program7-2.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>System Testing</title>
+</head>
+<body>
+<h1 align="center">Welcome to our home page</h1>
+<h3 align="center">You are logged in successfully</h3>
+<center>
+<a href="login.php">Go to Login</a>
+</center>
+</body>
+</html>
