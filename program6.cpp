f40cc43aadafc5df14e4d13b0008992e1b8382ca
nextbox <giridharank790@gmail.com>
2026-03-10 12:04:30 +0530

    Add discount calculation based on purchase amount
    Implement boundary values analysis for purchase discounts.

---
+++ program6.cpp
@@ -0,0 +1,26 @@
+6.BOUNDARY VALUES ANALYSIS
+#include <iostream.h>
+#include <conio.h>
+void main(){
+float amt, p;
+clrscr();
+cout<< "\n Enter the Purchase amount...";
+cin>> amt;
+if $(amt>1\&8~amt<5000)\{$
+cout<<"\n No discount for purchase below 5000";
+cout<<"\n Thank you.. Visit again";
+}else if $(amt>=500088~amt<10000)$
+$\{p=amt^{*}5/100;$
+cout<<"\n Your purchase has 5% discount";
+cout<<"\n Your discount amount:"<<p;
+cout<<"\n Thank you... Visit again";
+}else if (amt >=10000 && amt < 15000)
+$\{p=amt^{*}10/100;$
+cout<<"\n Your purchase has 10% discount";
+cout<<"\n Your discount amount:"<<p;
+cout<<"\n Thank you... Visit again";
+}else if $(amt>15000)$
+{cout<<"\n Purchase value exceeds";
+}else{
+cout<<"\n Invalid Purchase";}
+getch();}
