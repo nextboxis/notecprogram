391b77e8847329dba1f89ae219df730dd0f5c19d
nextbox <giridharank790@gmail.com>
2026-03-10 20:35:11 +0530

    Fix syntax errors and improve EB bill calculation
    Signed-off-by: nextbox <giridharank790@gmail.com>

--- program2.cpp
+++ program2.cpp
@@ -1,4 +1,4 @@
-2.DATA FLOW TESTING
+
 #include<iostream.h>
 #include<conio.h>
 void main() {
@@ -9,9 +9,9 @@ cout<<"\n Enter the units consumed:";
 cin>>units;
 if(units<=100) {
 cout<<"\n There is no charge";
-} else if(units>100 && units $<=200$) {
+} else if(units>100 && units <=200) {
 a=units-100;
-$b=a^{*}1.5;$
+b=a^{*}1.5;
 cout<<"\n Your EB Bill:"<<; }
 else if(units>200 && units<=500) {
 a=units-100;
@@ -23,11 +23,11 @@ cout<<"\n Your EB bill :"<<e;
 } else if(units>500) {
 a=units-100;
 b=a-100;
-$c=100^{*}3.5;$
+$c=100^{*}3.5;
 d=b*4.6;
 e=a-b;
-$f=e^{*}6.6;$
-$h=c+d+f$
+f=e^{*}6.6;
+h=c+d+f
 cout<<"\n Your EB bill:"<<h;
 }else{
 cout<<"\n Error in units";
