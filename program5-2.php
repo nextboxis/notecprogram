59ff9a762f0a0c081ee89c84dff3632eb2865303
nextbox <giridharank790@gmail.com>
2026-03-10 22:23:56 +0530

    Fix welcome message and link in program5-2.php
    Signed-off-by: nextbox <giridharank790@gmail.com>

--- program5-2.php
+++ program5-2.php
@@ -1,13 +1,12 @@
-CheckLogin Welcome.php
 <html>
 <head>
 <title>INTEGRATION TESTING</title>
 </head>
 <body>
-<?php echo "<BR> <h1 align='center'>WELCOME TO OUR HOME
- PAGE</h1></BR>
-<h3 align='center' >Your are Logged in Sucessfully ....</h3><BR>";
-echo "<a href='checkLogin.php'>Goto Login</a>";
+<?php 
+echo "<BR> <h1 align='center'>WELCOME TO OUR HOME PAGE</h1></BR>
+<h3 align='center'>Your are Logged in Successfully ....</h3><BR>";
+echo "<a href='html.php'>Goto Login</a>";
 ?>
 </body>
 </html>
