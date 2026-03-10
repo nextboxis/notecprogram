75098a59570f0cb2b07d97d3d49d4c9d44b7ea80
nextbox <giridharank790@gmail.com>
2026-03-10 12:03:46 +0530

    Add welcome page for integration testing

---
+++ program5-2.php
@@ -0,0 +1,13 @@
+CheckLogin Welcome.php
+<html>
+<head>
+<title>INTEGRATION TESTING</title>
+</head>
+<body>
+<?php echo "<BR> <h1 align='center'>WELCOME TO OUR HOME
+ PAGE</h1></BR>
+<h3 align='center' >Your are Logged in Sucessfully ....</h3><BR>";
+echo "<a href='checkLogin.php'>Goto Login</a>";
+?>
+</body>
+</html>
