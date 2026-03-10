c9e50cfb4f7698590c46911ec7c3363058239b04
nextbox <giridharank790@gmail.com>
2026-03-10 20:36:06 +0530

    Fix attendance condition syntax in program3.cpp
    Corrected syntax errors in attendance conditions.
    
    Signed-off-by: nextbox <giridharank790@gmail.com>

--- program3.cpp
+++ program3.cpp
@@ -1,4 +1,4 @@
-3.BRANCH TESTING
+
 #include<iostream.h>
 #include<conio.h>
 void main(){
@@ -6,14 +6,14 @@ int attend;
 clrscr();
 cout<<"\n Enter Attendance:";
 cin>>attend;
-if(attend> $>=75$ && attend< $\epsilon=100$) {
+if(attend> >=75$ && attend< \epsilon=100) {
 cout<<"\nYou are in PROFORMA I ";
 cout<<"\nYou are eligible to write all the exams";
-}else if (attend> $=65$ && attend<=74) {
+}else if (attend> =65 && attend<=74) {
 cout<<"\n You are in PROFORMA II";
 cout<<"\n You are allowed to write the exam with the permission of the
  principal."; }
-else if (attend> $>=55$ && attend<=64) {
+else if (attend> >=55 && attend<=64) {
 cout<<"\n You are in PROFORMA III";
 cout<<"\n You can't write current semester but you can write arrear
  exam";
