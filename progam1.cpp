68ec114fc8133fb4e9168138b29927d2d7716478
nextbox <giridharank790@gmail.com>
2026-03-10 11:56:41 +0530

    Create progam1.cpp

---
+++ progam1.cpp
@@ -0,0 +1,19 @@
+1.CONTROL FLOW TESTING
+#include<iostream.h>
+#include<conio.h>
+void main(){
+clrscr();
+int sum $=0,i$, $j=60$;
+cout<<"Enter the I value :";
+cin>>i;
+while $(j<=j)\{$
+$if(i\%2==0)$
+{
+cout<<i<<"\n";
+sum=sum+i;
+}
+i++;
+}
+cout<<"Result :"<<sum;
+getch();
+}
